caaa3c435744368199b850deaa28d3ec09922a5d
chaitanyakotagiri27 <chaitanyakotagiri1@gmail.com>
2025-09-18 00:02:37 +1000

    frontend: Order on port 80 (no :8001)

--- frontend/main.js
+++ frontend/main.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // These ports (30000 for Product, 30001 for Order) are mapped
     // from the Docker containers to the host machine in docker-compose.yml for Example 2.
     const PRODUCT_API_BASE_URL = 'http://20.53.102.217';
-    const ORDER_API_BASE_URL = 'http://20.53.103.38:8001';
+    const ORDER_API_BASE_URL = 'http://20.53.103.38';
 
     // Product Service is named 'product-service-w04e2' and exposes port 8000 internally.
     //const PRODUCT_API_BASE_URL = 'http://20.53.102.217';
     // Order Service is named 'order-service-w04e2' and exposes port 8001 internally.
-    //const ORDER_API_BASE_URL = 'http://20.53.103.38:8001';
+    //const ORDER_API_BASE_URL = 'http://20.53.103.38';
 
     // DOM Elements
     const messageBox = document.getElementById('message-box');
@@ -426,4 +426,5 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchProducts();
     fetchOrders();
 });
+
 
